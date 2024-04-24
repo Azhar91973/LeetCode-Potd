c366eab24606def44fe1f76c5a0636bea146ba38
Azhar Qureshi <azhar91973@gmail.com>
2024-04-24 07:36:21 +0530

    BFS

--- 310MinimumHeightTrees.cpp
+++ 310MinimumHeightTrees.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
+
+    /*
+        Idea is to remove all the leaf node's why?
+        Bcz those node which have higher indegree having min height so eliminating the all smaller indegree nodes 
+                
+    */
+
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
         vector<int> indegree(n,0);
+        // Making adj list and storing the indegree of each node
         for(int i=0;i<edges.size();i++)
         {
             adj[edges[i][0]].push_back(edges[i][1]);
@@ -12,7 +20,7 @@ public:
         }
 
         queue<int> q;
-
+        // inset all the leaf nodes
         for(int i=0;i<n;i++)
         {
             if(indegree[i] == 1)
@@ -22,6 +30,7 @@ public:
             }
         }
         vector<int> ans;
+        // bfs
         while(!q.empty())
         {
             int len = q.size();
